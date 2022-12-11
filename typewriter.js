63bb3359c3dc1539b5e658fdf419da982cceaac4
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-12-11 17:02:18 +0000

    Refactored to use a function

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,14 @@
 //Takes a sentence and displays it as an animation, ie each character is displayed separately with a delay from the previous character
-const sentence = "hello there from lighthouse labs";
 
-let i = 0;
-for (const char of (sentence + '\n')) {
-  setTimeout(() => {
-    process.stdout.write(char);
-  }, 50 * i);
-  i++;
-}
+const typeWriter = function(sentence) {
+  let i = 0;
+  for (const char of (sentence + '\n')) {
+    setTimeout(() => {
+      process.stdout.write(char);
+    }, 50 * i);
+    i++;
+  }
+};
+
+const sentence = "hello there from lighthouse labs";
+typeWriter(sentence);
